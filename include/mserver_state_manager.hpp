ee2aed34b805da77749637869f14452122244933
Jueast <jueastwang@outlook.com>
2017-12-18 01:30:31 +0800

    some improvement

--- include/mserver_state_manager.hpp
+++ include/mserver_state_manager.hpp
@@ -1,4 +1,5 @@
 #include "mpack.pb.h"
+#include <deque>
 #include <boost/asio.hpp>
 namespace MState {
 
@@ -8,7 +9,7 @@ struct Attribute {
 };
 
 typedef std::unordered_map<uint32_t, Attribute> Transaction;
-
+typedef std::deque<MNet::Mpack> TaskQueue;
 
     
 
@@ -21,13 +22,16 @@ class StateManager {
 public:
     static StateManager& getStateMgr();
     void init(const char* filename);   
-    
+	void sync();
+    void query();
+	void modify();
 private:
     StateManager() = default;
     ~StateManager() = default;
-    std::unordered_map<uint32_t, MState::Transaction> PlayerState;
-    MState::Transaction WorldState;
-    MNet::Mpack SyncMessage;
+    std::unordered_map<uint32_t, MState::Transaction> player_state_;
+    MState::Transaction world_state_;
+	MState::TaskQueue queue_;
+    MNet::Mpack sync_message_;
 };
 
 
